80d10b33136ec2f12a890e9310803882cc08d9c1
NMMalika <94176433+NMMalika@users.noreply.github.com>
2024-12-31 13:37:34 +0300

    usestate and functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-
+import { useState } from "react";
+import "./App.css";
 import { Header } from "./components/Header";
 import './App.css';
 import { Footer} from "./components/Footer";
 
 function App() {
+   const[count,setCount ]= useState(0);
+
+  function handleAdd() {
+    setCount(count + 1);
+  }
+   function handleSub() {
+     setCount(count -1);
+   }
+    function handleReset() {
+      setCount(0);
+    }
+
   return (
     <>
       <Header />
       <div className="App">will i one day become a pro</div>
+      <div className="box">
+        <p>{count}</p>
+        <button onClick={handleAdd} className="add">ADD</button>
+        <button onClick={handleSub } className="sub">SUB</button>
+        <button onClick={handleReset} className="reset">RESET</button>
+      </div>
       <Footer/>
     </>
   );
