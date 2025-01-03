3d36cb740b962c536dc913a53f4b206bed7e2c0d
NMMalika <94176433+NMMalika@users.noreply.github.com>
2025-01-03 21:50:57 +0300

    state working with strings

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { Footer} from "./components/Footer";
 
 function App() {
    const[count,setCount ]= useState(0);
+   const [tasks, setTasks] = useState([
+     { id: 1, name: "Record React Lectures", completed: false },
+     { id: 2, name: "edit React Lectures", completed: true },
+     { id: 3, name: "watch React Lectures", completed: false },
+   ]);
 
   function handleAdd() {
     setCount(count + 1);
@@ -20,14 +25,36 @@ function App() {
   return (
     <>
       <Header />
-      <div className="App">will i one day become a pro</div>
-      <div className="box">
-        <p>{count}</p>
-        <button onClick={handleAdd} className="add">ADD</button>
-        <button onClick={handleSub } className="sub">SUB</button>
-        <button onClick={handleReset} className="reset">RESET</button>
+      <div className="App">
+        <h1>will i one day become a pro</h1>
+        <div className="box">
+          <p>{count}</p>
+          <button onClick={handleAdd} className="add">
+            ADD
+          </button>
+          <button onClick={handleSub} className="sub">
+            SUB
+          </button>
+          <button onClick={handleReset} className="reset">
+            RESET
+          </button>
+        </div>
+        <div>
+          <h1>Task List</h1>
+          <ul>
+            {tasks.map((task, index) => (
+              <li key={index}>
+                <span>
+                  {task.id} - {task.name}
+                </span>
+                <button className="delete"> Delete </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <Footer/>
+
+      <Footer />
     </>
   );
 }
